11023b3f550f77b1f2854a4ee67f5f047a2da4a1
Alex33400 <alex.bregeon17@gmail.com>
2016-04-12 15:41:28 +0200

    US01 - Inscription
    Ajout du style au formulaire d'inscription
    Ajout de controles
    Branchement aux WS

--- public/javascripts/users/userSignUp.js
+++ public/javascripts/users/userSignUp.js
@@ -6,12 +6,32 @@ var app = angular.module('sample', []);
 
 app.controller("SignCtrl", function($scope, $http) {
 
-	$scope.addClient = function(user) {
+	$scope.addClient = function() {
 		var res = $http({
 			method : 'POST',
 			url : '/ws-users/sign-up',
-			data : user
+			data : $scope.user
 		})
-		
 	}
+
+	$scope.checkEmail = function() {
+		console.log($scope.user.email);
+	}
+})
+.directive("compareTo", function() {
+	return {
+		require: "ngModel",
+		scope: {
+			otherModelValue: "=compareTo"
+		},
+		link: function(scope, element, attributes, ngModel) {
+			ngModel.$validators.compareTo = function(modelValue) {
+				return modelValue == scope.otherModelValue;
+			};
+
+			scope.$watch("otherModelValue", function() {
+				ngModel.$validate();
+			});
+		}
+	};
 });
--- routes/users/users.js
+++ routes/users/users.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/sign-up', function(req, res, next) {
-	res.render('users/signUp', { title: 'Express' });
+	res.render('users/signUp', { title: 'Inscription utilisateur' });
 });
 
 router.post('/sign-up', function(req, res, next) {
