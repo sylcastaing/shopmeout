1a4cc9fb561f27941fd8537675b167e6c635c113
Alex33400 <alex.bregeon17@gmail.com>
2016-04-13 11:11:18 +0200

    US01 - Inscription
    Validation des données formulaire

--- public/javascripts/users/userSignUp.js
+++ public/javascripts/users/userSignUp.js
@@ -1,57 +1,58 @@
 /**
  * Déclaration de l'application demoApp
  */
-var app = angular.module('sample', []);
+ var app = angular.module('sample', []);
 
 
-app.controller("SignCtrl", function($scope, $http) {
+ app.controller("SignCtrl", function($scope, $http) {
 
 
-	$scope.addClient = function() {
-		var res = $http({
-			method : 'POST',
-			url : '/ws-users/sign-up',
-			data : $scope.user
-		}).success(function (data, status, headers, config){
-			if (status == '200') {
-				console.log("succes");
-				console.log(data);
+ 	$scope.addClient = function() {
+ 		var res = $http({
+ 			method : 'POST',
+ 			url : '/ws-users/sign-up',
+ 			data : $scope.user
+ 		}).success(function (data, status, headers, config){
+			console.log("succes");
+			console.log(data);
+ 		}).error(function (data, status, headers, config){
+ 			console.log("echec");
+ 		});
+ 		console.log($scope.user);
+ 	}
+
+ 	$scope.checkEmail = function() {
+ 		//console.log($scope.user.motDePasse)
+ 		var res = $http({
+ 			method : 'POST',
+ 			url : '/ws-users/check-email',
+ 			data : { "email" : $scope.user.email}
+ 		}).success(function (data, status, headers, config){
+			if(data.statut==false && data.erreur!="E-mail vide à la récupération.") {
+				$scope.signup.email.$error.emailPrise = true;
 			}
-		}).error(function (data, status, headers, config){
-			console.log("echec");
-		});
-		console.log($scope.user);
-	}
-
-	$scope.checkEmail = function() {
-		var res = $http({
-			method : 'POST',
-			url : '/ws-users/check-email',
-			data : { "email" : $scope.user.email}
-		}).success(function (data, status, headers, config){
-			if (status == '200') {
-				console.log(data);
+			else {
+				$scope.signup.email.$error.emailPrise = false;
 			}
 		}).error(function (data, status, headers, config){
 			console.log(data);
-			ngModel.$setValidity("emailPrise",false);
 		});
 	}
 })
-.directive("compareTo", function() {
-	return {
-		require: "ngModel",
-		scope: {
-			otherModelValue: "=compareTo"
-		},
-		link: function(scope, element, attributes, ngModel) {
-			ngModel.$validators.compareTo = function(modelValue) {
-				return modelValue == scope.otherModelValue;
-			};
+ .directive("compareTo", function() {
+ 	return {
+ 		require: "ngModel",
+ 		scope: {
+ 			otherModelValue: "=compareTo"
+ 		},
+ 		link: function(scope, element, attributes, ngModel) {
+ 			ngModel.$validators.compareTo = function(modelValue) {
+ 				return modelValue == scope.otherModelValue;
+ 			};
 
-			scope.$watch("otherModelValue", function() {
-				ngModel.$validate();
-			});
-		}
-	};
-});
+ 			scope.$watch("otherModelValue", function() {
+ 				ngModel.$validate();
+ 			});
+ 		}
+ 	};
+ });
