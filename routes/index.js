abc6e4cfe0ffe4e75b7bb8c48e4756c9959a4d4b
roxxcsn <roxannecousin@gmail.com>
2016-04-14 10:48:54 +0200

    US08
    Ajout Footer + Mentions légales

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+router.get('/mentions-legales', function(req, res, next) {
+	res.render('mentionslegales', { title: 'Mentions Légales' });
+});
+
 module.exports = router;
