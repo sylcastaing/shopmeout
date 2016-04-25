03f4f6f0537441c4327022bf72d1d77899f06831
Alex33400 <alex.bregeon17@gmail.com>
2016-04-25 11:43:17 +0200

    Correction d'un bug sur checkEmail

--- public/javascripts/users/userSignUp.js
+++ public/javascripts/users/userSignUp.js
@@ -2,66 +2,69 @@
 
  app.controller("SignCtrl", function($scope, $http) {
 
-	 $scope.removeError = function() {
-	 	if($scope.signup.$error.sexeError) {
-	 		$scope.signup.$error.sexeError = false;
-	 	}
-	 }
+ 	$scope.removeError = function() {
+ 		if($scope.signup.$error.sexeError) {
+ 			$scope.signup.$error.sexeError = false;
+ 		}
+ 	}
 
  	$scope.addClient = function() {
 
  		if($scope.myModel == undefined) {
-			$scope.signup.$error.sexeError = true;
+ 			$scope.signup.$error.sexeError = true;
  		}
  		else {
  			$scope.signup.$error.sexeError = false;
  			$scope.signup.$invalid = false;
- 		
-	 		$scope.user.motDePasse = CryptoJS.SHA1($scope.motDePasse).toString();
-	 		$scope.user.sexe = $scope.myModel['sexe'];
-	 		var res = $http({
-	 			method : 'POST',
-	 			url : '/ws-users/sign-up',
-	 			data : $scope.user
-	 		}).success(function (data, status, headers, config){
-	 			if(data.statut) {
-	 				var autoCo = $http({
-						method : 'POST',
-						url : '/ws-users/sign-in',
-						data : { "email" : $scope.user.email, "motDePasse" : $scope.user.motDePasse }
-					}).success(function (data, status, headers, config){
-			 		}).error(function (data, status, headers, config){
-	 				});
-	 				document.location="/";
-	 			}
-	 			else {
-	 				$scope.signup.$signUpError = true;
-	 				$scope.signup.$signUpErrorMsg = data.err;
-	 			}
-	 		}).error(function (data, status, headers, config){
-	 			$scope.signup.$signUpError = true;
+
+ 			$scope.user.motDePasse = CryptoJS.SHA1($scope.motDePasse).toString();
+ 			$scope.user.sexe = $scope.myModel['sexe'];
+ 			var res = $http({
+ 				method : 'POST',
+ 				url : '/ws-users/sign-up',
+ 				data : $scope.user
+ 			}).success(function (data, status, headers, config){
+ 				if(data.statut) {
+ 					var autoCo = $http({
+ 						method : 'POST',
+ 						url : '/ws-users/sign-in',
+ 						data : { "email" : $scope.user.email, "motDePasse" : $scope.user.motDePasse }
+ 					}).success(function (data, status, headers, config){
+ 					}).error(function (data, status, headers, config){
+ 					});
+ 					document.location="/";
+ 				}
+ 				else {
+ 					$scope.signup.$signUpError = true;
+ 					$scope.signup.$signUpErrorMsg = data.err;
+ 				}
+ 			}).error(function (data, status, headers, config){
+ 				$scope.signup.$signUpError = true;
  				$scope.signup.$signUpErrorMsg = data.err;
-	 		});
+ 			});
  		}
  	}
 
  	$scope.checkEmail = function() {
- 		var res = $http({
- 			method : 'POST',
- 			url : '/ws-users/check-email',
- 			data : { "email" : $scope.user.email}
- 		}).success(function (data, status, headers, config){
- 			if(data.statut==false && data.erreur!="E-mail vide à la récupération.") {
- 				$scope.signup.email.$invalid = true;
- 				$scope.signup.email.$error.emailPrise = true;
- 				$scope.signup.email.$error.emailPriseMessage = data.err;
- 			}
- 			else {
- 				$scope.signup.email.$error.emailPrise = false;
- 			}
- 		}).error(function (data, status, headers, config){
- 		});
+ 		if($scope.signup.email.modelValue != undefined) {
+ 			var res = $http({
+ 				method : 'POST',
+ 				url : '/ws-users/check-email',
+ 				data : { "email" : $scope.user.email}
+ 			}).success(function (data, status, headers, config){
+ 				if(data.statut==false && data.erreur!="E-mail vide à la récupération.") {
+ 					$scope.signup.email.$invalid = true;
+ 					$scope.signup.email.$error.emailPrise = true;
+ 					$scope.signup.email.$error.emailPriseMessage = data.err;
+ 				}
+ 				else {
+ 					$scope.signup.email.$error.emailPrise = false;
+ 				}
+ 			}).error(function (data, status, headers, config){
+ 			});
+ 		}
  	}
+
  })
  .directive("compareTo", function() {
  	return {
@@ -93,9 +96,9 @@
 
  			$scope.$watch(attr.ngModel, function(newValue, oldValue) {
  				element.parent(".btn-group").find('button').removeClass("active");
-				element.parent(".btn-group") //.children()
-				.find("button[value='" + newValue + "']").addClass('active');
-			});
+					element.parent(".btn-group") //.children()
+					.find("button[value='" + newValue + "']").addClass('active');
+				});
  		}
  	};
  });
