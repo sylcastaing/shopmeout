9b4d44e4b19bc0b0b6efaf33a5448d382eec6707
NasrC <nasr.chadli@gmail.com>
2016-05-04 15:48:10 +0200

    Revert "US10 commit to update"
    This reverts commit 121eda6bab4507cb27553edbeda59a2fd8f01d7a.

--- public/javascripts/needShop/needShop.js
+++ public/javascripts/needShop/needShop.js
@@ -36,7 +36,6 @@ app.controller("NeedShopCtrl", function($scope, $http) {
 			$scope.needShop.$error.NbArticleError = false;
  			$scope.needShop.dateShopping.$invalid = false;
  			$scope.needShop.adresse.$invalid = false;
- 			$scope.needShop.$invalid = false;
 			$scope.data.magasin = $scope.selectedMagasin;
 			$scope.data.adresseMagasin = $scope.adresseSelectedMagasin;
 		var res = $http({
@@ -61,7 +60,6 @@ app.controller("NeedShopCtrl", function($scope, $http) {
 				}
 				});
 			}
-
 		}).error(function (data, status, headers, config){
 			$scope.needShop.$error.addNeedShop = true;
 			$scope.needShop.$error.message = "Problème serveur";
